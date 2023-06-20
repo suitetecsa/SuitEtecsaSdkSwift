1f2173c5491dd1e94dbb65a65c34a91cea2bb345
lesclaz <lesclaz95@gmail.com>
2023-06-20 18:56:54 -0400

    wip

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SuitEtecsaSdkSwift",
-            targets: ["SuitEtecsaSdkSwift"]),
+            targets: ["SuitEtecsaSdkSwift"])
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.4")),
@@ -19,10 +19,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SuitEtecsaSdkSwift", dependencies: ["Alamofire", "SwiftSoup"]),
+            name: "SuitEtecsaSdkSwift",dependencies: ["Alamofire", "SwiftSoup"]),
         .testTarget(
             name: "SuitEtecsaSdkSwiftTests",
             dependencies: ["SuitEtecsaSdkSwift"]),
-    ],
-    version: "1.0.0"
+    ]
 )
