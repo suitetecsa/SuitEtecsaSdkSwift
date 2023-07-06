a54921584c9121e52ac9d4f99c653b197ad48081
lesclaz <lesclaz95@gmail.com>
2023-07-06 03:24:58 -0400

    ios 14 support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SuitEtecsaSdkSwift",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
